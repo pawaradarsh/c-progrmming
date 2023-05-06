1f3218f6cb4fe37754d1696abf2b3948632d55a7
pawaradarsh <132763527+pawaradarsh@users.noreply.github.com>
2023-05-06 20:04:39 +0530

    Add files via upload

---
+++ Demo.s
@@ -0,0 +1,55 @@
+	.file	"Demo.c"
+	.section .rdata,"dr"
+LC0:
+	.ascii "Jay Ganesh...\0"
+	.text
+	.globl	_display
+	.def	_display;	.scl	2;	.type	32;	.endef
+_display:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	$LC0, (%esp)
+	call	_puts
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC1:
+	.ascii "Inside Main.\0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB11:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	movl	$LC1, (%esp)
+	call	_puts
+	call	_display
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE11:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+	.def	_puts;	.scl	2;	.type	32;	.endef
